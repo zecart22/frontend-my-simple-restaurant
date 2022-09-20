dfa19401edc7133d7f35a0d059a159b5b1c1c7cf
zeca.rt22 <zeca.rt22@gmail.com>
2022-09-20 07:56:05 -0300

    fix/create category form

--- src/pages/CreateCategory/index.tsx
+++ src/pages/CreateCategory/index.tsx
@@ -1,4 +1,14 @@
-import { VStack, Text, Input, Button, useToast } from "@chakra-ui/react";
+import {
+  VStack,
+  Text,
+  Input,
+  useToast,
+  FormLabel,
+  FormControl,
+  Button,
+  FormHelperText,
+  FormErrorMessage,
+} from "@chakra-ui/react";
 import { Header } from "../../components/Header";
 import { Link, useHistory } from "react-router-dom";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -19,6 +29,20 @@ export const CreateCategory = () => {
   const toast = useToast();
   const token = localStorage.getItem("@AcessToken");
   const history = useHistory();
+  const [error, setError] = useState(false);
+  const [noClick, setNoClick] = useState(true);
+  const [categoryName, setCategoryName] = useState("");
+
+  const handleCategoryName = (e: any) => {
+    setCategoryName(e.target.value);
+  };
+
+  const handleNoClick = () => {
+    setNoClick(false);
+  };
+
+  const isErrorCategoryName = categoryName === "";
+  const isInvalid = isErrorCategoryName;
 
   const {
     formState: { errors: string },
@@ -48,15 +72,18 @@ export const CreateCategory = () => {
 
       .catch((err) => {
         console.log(err);
-        toast({
-          position: "top",
-          title: "Opss algo deu errado!! ",
-          description: err,
-          status: "warning",
-          duration: 2000,
-          isClosable: true,
-        });
+        setError(true);
       });
+    if (error) {
+      toast({
+        position: "top",
+        title: "Opss algo deu errado!! ",
+        description: "Tente novamente",
+        status: "error",
+        duration: 2000,
+        isClosable: true,
+      });
+    }
   };
 
   return (
@@ -64,25 +91,47 @@ export const CreateCategory = () => {
       <Header />
       <VStack mt={50} spacing={5} justifyContent={"center"}>
         <Text fontSize={30}>Nova Categoria</Text>
+
         <Input
+          isRequired
           w={["270px", "380px", "400px", "600px"]}
           h={"50px"}
           placeholder={"digite o nome da nova categoria"}
           border={"1px"}
           borderColor={"theme.gray50"}
           boxShadow={"md"}
+          isInvalid={isInvalid}
           {...register("name")}
+          value={categoryName}
+          onChange={handleCategoryName}
+          onClick={handleNoClick as any}
         />
 
-        <Button
-          w={["270px", "380px", "400px", "600px"]}
-          h={"50px"}
-          color={"theme.grafit"}
-          children={"Criar Categoria"}
-          bg={"theme.yellow"}
-          type={"submit"}
-          onClick={handleSubmit(handleCreate as any)}
-        />
+        {isInvalid ? (
+          <>
+            <Button
+              w={["270px", "380px", "400px", "600px"]}
+              h={"50px"}
+              color={"theme.grafit"}
+              children={"Criar Categoria"}
+              bg={"gray.100"}
+              type={"submit"}
+            />
+          </>
+        ) : (
+          <>
+            <Button
+              w={["270px", "380px", "400px", "600px"]}
+              h={"50px"}
+              color={"theme.grafit"}
+              children={"Criar Categoria"}
+              bg={"theme.yellow"}
+              type={"submit"}
+              onClick={handleSubmit(handleCreate as any)}
+            />
+          </>
+        )}
+
         <Link to={"/listcategory"}>
           <Text fontSize={20} color={"theme.red"}>
             Ver todas categorias
